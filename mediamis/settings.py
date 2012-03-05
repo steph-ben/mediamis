49f0f3c97da7b636a32788aa4bdb07046332296c
Steph <stephane.benchimol@gmail.com>
2012-03-05 11:41:33 +0100

    update settings

--- mediamis/settings.py
+++ mediamis/settings.py
@@ -45,6 +45,7 @@ MEDIA_ROOT = '/app/mediamis/media/'
 MEDIA_URL = 'http://soif.arcadio.info/mediamis/media/'
 
 
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
