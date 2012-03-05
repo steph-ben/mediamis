9c805041bb7c34837c9539d850a0b224f15e2130
Steph <stephane.benchimol@gmail.com>
2012-03-05 11:08:26 +0100

    update settings

--- mediamis/settings.py
+++ mediamis/settings.py
@@ -48,13 +48,13 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
-SERVE_STATIC_FILES = True
+SERVE_STATIC_FILES = False
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -116,7 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-#    'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'django.contrib.admin',
     'django.contrib.admindocs',
 
@@ -181,7 +181,7 @@ DATABASES = {
 
 
 # Dev server serve /media files
-SERVE_STATIC_FILES = True
+SERVE_STATIC_FILES = False
 
 
 # For debug toolbar
