ce9aa64edcfe77a14a3828ca43f808b89ea0fa88
Steph <stephane.benchimol@gmail.com>
2012-03-05 10:49:09 +0100

    update settings

--- mediamis/settings.py
+++ mediamis/settings.py
@@ -1,4 +1,6 @@
-# Django settings for hellodjango project.
+import os
+import settings
+# Django settings for myfriendlylibrary project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,16 +11,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'plop',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -45,12 +37,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/app/mediamis/media/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -62,6 +54,8 @@ STATIC_ROOT = ''
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
 
+SERVE_STATIC_FILES = True
+
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
@@ -72,6 +66,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/app/mediamis/media'
 )
 
 # List of finder classes that know how to find static files in
@@ -83,7 +78,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'u()f#xqvv*147lfp^59v^6w4lo+8!0bfsurxkkrz&ffln=ml#y'
+SECRET_KEY = 'j&gh_7)d1s2aahl=^!8f790o%8^7!!dp^pg17x7#ut@zxl@2nx'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -92,21 +87,28 @@ TEMPLATE_LOADERS = (
 #     'django.template.loaders.eggs.Loader',
 )
 
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.request",
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), "../templates"),
+)
+
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'registration.middleware.LoginFormMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -114,11 +116,22 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
+#    'django.contrib.staticfiles',
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
+
+    # Useful apps for dev
+    'django_extensions',    # Add several commands to manage.py
+    'south',                # Manage database changes easily (eg. new columns)
+    'autofixture',
+
+    # Thirds apps
+    'djeneralize',
+
+    # Reusable apps
+    'registration',
+
+    'friendlib',
 )
 
 # A sample logging configuration. The only tangible logging
@@ -142,4 +155,40 @@ LOGGING = {
             'propagate': True,
         },
     }
+}
+
+from default_settings import *
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+# See debug_toolbar/panels/sql.py
+SQL_DEBUG = False
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'mediamis',                      # Or path to database file if using sqlite3.
+        'USER': 'mediamis',                      # Not used with sqlite3.
+        'PASSWORD': 'mediamis',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+
+# Dev server serve /media files
+SERVE_STATIC_FILES = True
+
+
+# For debug toolbar
+INSTALLED_APPS += ('debug_toolbar',)
+INTERNAL_IPS = ('127.0.0.1',)
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+DEBUG_TOOLBAR_CONFIG = {
+    'INTERCEPT_REDIRECTS': False,
+    'HIDE_DJANGO_SQL': True,
 }
