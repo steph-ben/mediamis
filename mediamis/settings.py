fb8f2b6c16bfa05ef5297a80ec4c8aacf648ab65
Steph <stephane.benchimol@gmail.com>
2012-03-05 11:41:02 +0100

    plop

--- mediamis/settings.py
+++ mediamis/settings.py
@@ -44,6 +44,7 @@ MEDIA_ROOT = '/app/mediamis/media/'
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = 'http://soif.arcadio.info/mediamis/media/'
 
+
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
