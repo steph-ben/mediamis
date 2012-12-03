091e3fb6905bed6a4ec46468fa5eef3845a3de9d
Steph <stephane.benchimol@gmail.com>
2012-12-04 00:33:37 +0100

    Putain ...

--- mediamis/settings.py
+++ mediamis/settings.py
@@ -6,3 +6,13 @@ if platform.node() == 'steph-laptop':
 else:
     import settings_conf.heroku_settings as settings
 
+
+import dj_database_url
+
+if 'DATABASES' not in locals():
+    DATABASES = {}
+
+if not 'default' in DATABASES:
+    DATABASES['default'] = {}
+
+DATABASES['default'].update(dj_database_url.config(default='postgres://'))
