b435ffcbbbaab6d7215e2fdfca37132e57bcb814
Steph <stephane.benchimol@gmail.com>
2012-12-04 00:19:03 +0100

    Current path for static files

--- mediamis/settings.py
+++ mediamis/settings.py
@@ -1,7 +1,7 @@
 import platform
 import settings_conf
 
-if platform.node() == 'steph-laptop':
+if platform.node() == 'steph-laptopp':
     import settings_conf.local_settings as settings
 else:
     import settings_conf.heroku_settings as settings
