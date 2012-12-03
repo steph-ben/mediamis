2093ee8bcb7f72b40d67f131a1a84f5645793408
Steph <stephane.benchimol@gmail.com>
2012-12-04 00:27:10 +0100

    Good env ...

--- mediamis/settings.py
+++ mediamis/settings.py
@@ -1,7 +1,8 @@
 import platform
 import settings_conf
 
-if platform.node() == 'steph-laptopp':
+if platform.node() == 'steph-laptop':
     import settings_conf.local_settings as settings
 else:
     import settings_conf.heroku_settings as settings
+
