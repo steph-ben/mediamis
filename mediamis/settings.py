e9b92ebe5e782cb7be1de4f97931d0ca0530bccb
Steph <stephane.benchimol@gmail.com>
2012-12-03 23:45:09 +0100

    Having a nice settings, heroku compatible ...

---
+++ mediamis/settings.py
@@ -0,0 +1,7 @@
+import platform
+import settings_conf
+
+if platform.node() == 'steph-laptop':
+    import settings_conf.local_settings as settings
+else:
+    import settings_conf.heroku_settings as settings
